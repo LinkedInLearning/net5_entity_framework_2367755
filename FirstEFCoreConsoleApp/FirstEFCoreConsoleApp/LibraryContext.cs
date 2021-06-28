1e71400d3b0d3047d37712790b102cead0b3ba10
Juanjo <juanjo@jmontiel.es>
2021-06-29 01:02:29 +0200

    Pequeña aplicación de ejemplo para entender cómo funciona EF Core, con las clases de entidad y el contexto.

---
+++ FirstEFCoreConsoleApp/FirstEFCoreConsoleApp/LibraryContext.cs
@@ -0,0 +1,25 @@
+﻿using FirstEFCoreConsoleApp.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEFCoreConsoleApp
+{
+	public class LibraryContext : DbContext
+	{
+
+		public DbSet<Author> Authors { get; set; }
+
+		public DbSet<Book> Books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+			optionsBuilder.UseSqlite($"Data Source={Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), "library.db")}");
+	}
+}
