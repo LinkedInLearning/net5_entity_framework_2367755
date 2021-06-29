328faf52ec835de6b51abf2c8e1b4119fe7f9644
Juanjo <juanjo@jmontiel.es>
2021-06-29 22:41:28 +0200

    Añado migración inicial.

--- FirstEFCoreConsoleApp/FirstEFCoreConsoleApp/Program.cs
+++ FirstEFCoreConsoleApp/FirstEFCoreConsoleApp/Program.cs
@@ -11,14 +11,12 @@ namespace FirstEFCoreConsoleApp
 		static async Task Main(string[] args)
 		{
 			using var context = new LibraryContext();
-			context.Database.EnsureCreated();
-			/*
-			 * var author = new Author { Name = "Stephen", LastName = "King" };
+			// context.Database.EnsureCreated();
+			 var author = new Author { Name = "Stephen", LastName = "King" };
 			var book = new Book { Title = "Los ojos del dragón", Sinopsis = "Prueba de un libro.", Author = author };
 			context.Authors.Add(author);
 			context.Books.Add(book);
 			await context.SaveChangesAsync();
-			*/
 
 			var bookFromDb = context.Books.FirstOrDefault(b => b.Title == "Los ojos del dragón");
 		}
