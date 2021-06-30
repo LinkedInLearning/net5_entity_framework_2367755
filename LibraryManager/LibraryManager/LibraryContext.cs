6bf9d1c2f01f43fb09eded32598cbddbe0f8e365
Juanjo <juanjo@jmontiel.es>
2021-06-30 13:30:17 +0200

    Añado una aplicación de consola de ejemplo par explicar cómo en aplicaciones de consola sencillas se podría utilizar un contexto de larga duración.

---
+++ LibraryManager/LibraryManager/LibraryContext.cs
@@ -0,0 +1,33 @@
+﻿using FirstEFCoreConsoleApp.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEFCoreConsoleApp
+{
+	public class LibraryContext : DbContext
+	{
+
+		public DbSet<Author> Authors { get; set; }
+
+		public DbSet<Book> Books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FirstEFCoreConsoleApp", "library.db");
+			var dbDir = Path.GetDirectoryName(dbPath);
+			if (!Directory.Exists(dbDir))
+			{
+				Directory.CreateDirectory(dbDir);
+			}
+			optionsBuilder.UseSqlite($"Data Source={dbPath}");
+		}
+	}
+}
