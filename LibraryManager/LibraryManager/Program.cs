030a70cca24e2bcc68d017f45bc89146ce8fcdd1
Juanjo <juanjo@jmontiel.es>
2021-07-02 06:14:43 +0200

    Hago que en la aplicación de consola se intente usar un contexto único con distintas operaciones en paralelo para enseñar los errores producidos en este escenario.

--- LibraryManager/LibraryManager/Program.cs
+++ LibraryManager/LibraryManager/Program.cs
@@ -3,6 +3,7 @@
 using Microsoft.EntityFrameworkCore;
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,131 +11,38 @@ namespace FirstEFCoreConsoleApp
 {
 	class Program
 	{
-		private static LibraryContext _context;
 
 		static async Task<int> Main(string[] args)
 		{
-			_context = new LibraryContext();
-
-			var exit = false;
-
-			while (!exit)
+			try
 			{
-				ShowMainMenu();
-				var choice = ReadMenuOption();
-				switch (choice)
+				using var context = new LibraryContext();
+				var authors = new[]
 				{
-					case 3:
-						exit = true;
-						break;
-					case 1:
-						await ManageAuthorsAsync();
-						break;
-					case 2:
-						await ManageBooksAsync();
-						break;
-					default:
-						Console.WriteLine("Opción incorrecta.");
-						break;
+					new Author { Name = "Robert", LastName = "Cecil Martin" },
+					new Author { Name = "Martin", LastName = "Fowler" }
+				};
+				
+				var tasks = new List<Task>();
+				for (var i = 0; i < authors.Length; i++)
+				{
+					var author = authors[i];
+					tasks.Add(Task.Run(async () =>
+					{
+						await context.AddAsync(author);
+						await context.SaveChangesAsync();
+						context.Authors.Remove(author);
+						await context.SaveChangesAsync();
+					}));
 				}
+				await Task.WhenAll(tasks);
 			}
-			await _context.DisposeAsync();
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
 			return 0;
-		}
-
-		private static void ShowMainMenu()
-		{
-			Console.WriteLine("1. Autores." + Environment.NewLine +
-				"2. Libros." + Environment.NewLine +
-				"3. Salir.");
-		}
-
-		private static int ReadMenuOption()
-		{
-			var choice = Console.ReadLine();
-			int choiceNumber = 0;
-			while (!Int32.TryParse(choice, out choiceNumber))
-			{
-				Console.WriteLine("Debes especificar un valor numérico.");
-			}
-			return choiceNumber;
-		}
-
-		private static async Task ManageAuthorsAsync()
-		{
-			var exit = false;
-			while (!exit)
-			{
-				Console.WriteLine(@"Menú de autores:
-1. Lista de autores.
-2. Añadir autor.
-3. Eliminar autor.
-4. Volver.");
-				var choice = ReadMenuOption();
-				switch (choice)
-				{
-					case 4:
-						exit = true;
-						break;
-					case 1:
-						await ShowAuthorsAsync();
-						break;
-					case 2:
-					case 3:
-						Console.WriteLine("NO implementado.");
-						break;
-				}
-			}
-		}
-
-		private static async Task ShowAuthorsAsync()
-		{
-			var authors = await _context.Authors.ToListAsync();
-			PrintRow(100, "Nombre", "Apellidos");
-			PrintLine(100);
-			foreach (var author in authors)
-			{
-				PrintRow(100, author.Name, author.LastName);
-			}
-			PrintLine(100);
-		}
-
-		private static async Task ManageBooksAsync()
-		{
-			Console.WriteLine("No implementado.");
-		}
-
-
-		private static void PrintLine(int tableWidth)
-		{
-			Console.WriteLine(new string('-', tableWidth));
-		}
-
-		private static void PrintRow(int tableWidth, params string[] columns)
-		{
-			int width = (tableWidth - columns.Length) / columns.Length;
-			string row = "|";
-
-			foreach (string column in columns)
-			{
-				row += AlignCentre(column, width) + "|";
-			}
-
-			Console.WriteLine(row);
-		}
-
-		private static string AlignCentre(string text, int width)
-		{
-			text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
-
-			if (string.IsNullOrEmpty(text))
-			{
-				return new string(' ', width);
-			}
-			else
-			{
-				return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
-			}
 		}
 	}
 }
