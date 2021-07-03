00bb83792411b85f154c8c1c00a097a1aa17078a
Juanjo <juanjo@jmontiel.es>
2021-07-04 01:10:28 +0200

    Añado dos campos de prueba que excluyo usando DAtaAnotations y Fluent API.

--- LibraryManagerWeb/LibraryManagerWeb/DataAccess/Author.cs
+++ LibraryManagerWeb/LibraryManagerWeb/DataAccess/Author.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,9 @@ namespace LibraryManagerWeb.DataAccess
 		public string LastName { get; set; }
 
 		public List<Book> Books { get; set; } = new List<Book>();
+
+		[NotMapped]
+		public DateTime LoadedDate { get; set; }
 
 	}
 }
